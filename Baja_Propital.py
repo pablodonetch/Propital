02c7e2d054759bdd4a0dc0267983c1930fbbeabc
Pablo Donetch <pablodonetch@gmail.com>
2023-01-18 15:38:33 -0300

    20230118_03

--- Baja_Propital.py
+++ Baja_Propital.py
@@ -1,4 +1,4 @@
-import time
+import time, math
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,10 +11,23 @@ password.send_keys("donetch$1245")
 driver.find_element("id", "botLogin").click()
 time.sleep(2)
 driver.get("https://propital.prop360.cl/backOffice/propiedades/propiedades.aspx?cor=1323")
-for i in range(0,2):
+print(driver.find_element(By.XPATH, "//*[@class='caption-subject font-green-sharp bold uppercase']").text)
+texto=driver.find_element(By.XPATH, "//*[@class='caption-subject font-green-sharp bold uppercase']").text.split(' ')
+propiedades=int(texto[0])
+print(propiedades)
+for i in range(1,propiedades):
     time.sleep(2)
+    lugar_prop=i
+    pagina=1
+    for j in range(1, (math.floor(i/20)+1)):
+        sig=driver.find_elements(By.XPATH, "//*[@title='Next']")
+        print(sig)
+        sig[0].click()
+        lugar_prop-=20
+        pagina+=1
+        time.sleep(1)
     boton=driver.find_elements(By.XPATH, "//*[@class='btn btn-xs default blue-stripe lnkEdit']")
-    boton[0].click()
+    boton[lugar_prop].click()
     time.sleep(2)
     boton=driver.find_elements(By.XPATH, "//*[@class='itemMenuMax']")
     boton[4].click()
@@ -29,5 +42,6 @@ for i in range(0,2):
         alert.accept()
     except:
         pass
+    print(f'propiedad {lugar_prop} de la pagina {pagina}')
     time.sleep(1)#espera
     driver.get("https://propital.prop360.cl/backOffice/propiedades/propiedades.aspx?cor=1323")#vuelve a la página inicial de la cartera
